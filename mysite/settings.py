c28a4aac4bcde6d46d08f5a6ad70fad8a640cd0d
cannavina98 <recannavina@hotmail.com>
2021-09-17 16:21:24 -0300

    a

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-ALLOWED_HOSTS = []
-#ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("bora-intra.herokuapp.com/", "127.0.0.1,localhost").split(",")
 
 # Application definition
 
