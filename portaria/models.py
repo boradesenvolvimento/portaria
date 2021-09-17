cb0064890781b89e7e5ba23b28973d98585b92f9
Renan Amarantes <renan.amarantes@bora.local>
2021-09-17 14:43:20 -0300

    added files and changes required for deployment

--- portaria/models.py
+++ portaria/models.py
@@ -4,17 +4,25 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
-
-
+TIPO_FUNC = (
+    ('INTERNO', 'INTERNO'),
+    ('AGREGADO', 'AGREGADO'),
+)
+TIPO_VIAGEM = (
+    ('COLETA', 'COLETA'),
+    ('ENTREGA', 'ENTREGA'),
+    ('TRANSF', 'TRANSF')
+)
 # Create your models here.
 class Cadastro(models.Model):
-    placa = models.CharField(max_length=20, primary_key=True)
+    placa = models.CharField(max_length=7)
     placa2 = models.CharField(max_length=20, blank=True)
     motorista = models.CharField(max_length=50)
-    empresa = models.IntegerField()
-    filial = models.IntegerField()
+    empresa = models.CharField(max_length=100)
     garagem = models.IntegerField()
-    hr_chegada = models.DateTimeField(blank=True)
+    tipo_func = models.CharField(max_length=10, choices=TIPO_FUNC)
+    tipo_viagem = models.CharField(max_length=10, choices=TIPO_VIAGEM)
+    hr_chegada = models.DateTimeField(blank=True, default=timezone.now)
     hr_saida = models.DateTimeField(blank=True, null=True)
     autor = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
 
