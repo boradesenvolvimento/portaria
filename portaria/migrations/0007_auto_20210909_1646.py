cf960c73ae467c2180868790625a3f429361a187
cannavina98 <recannavina@hotmail.com>
2021-09-17 15:47:39 -0300

    a

--- portaria/migrations/0007_auto_20210909_1646.py
+++ portaria/migrations/0007_auto_20210909_1646.py
@@ -10,6 +10,7 @@ class Migration(migrations.Migration):
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('portaria', '0006_alter_cadastro_hr_saida'),
+        ('portaria', '0006_alter_cadastro_hr_chegada')
     ]
 
     operations = [
@@ -23,5 +24,21 @@ class Migration(migrations.Migration):
             model_name='cadastro',
             name='hr_saida',
             field=models.DateTimeField(blank=True),
+        ),
+        migrations.AlterField(
+            model_name='cadastro',
+            name='hr_chegada',
+            field=models.DateTimeField(blank=True, default=django.utils.timezone.now),
+        ),
+        migrations.AlterField(
+            model_name='cadastro',
+            name='placa',
+            field=models.CharField(max_length=7),
+        ),
+        migrations.AlterField(
+            model_name='cadastro',
+            name='tipo_viagem',
+            field=models.CharField(choices=[('COLETA', 'COLETA'), ('ENTREGA', 'ENTREGA'), ('TRANSF', 'TRANSF')],
+                                   max_length=10),
         ),
     ]
