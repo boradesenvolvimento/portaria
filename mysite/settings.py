69dda46c67732a1544112bcd861f7fc56fb1d091
cannavina98 <recannavina@hotmail.com>
2021-09-20 07:52:24 -0300

    a

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-DEBUG = os.environ.get('DJANGO_DEBUG')
+DEBUG = os.environ.get('DJANGO_DEBUG', '')
 #ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ["bora-intra.herokuapp.com", "127.0.0.1"]
 
@@ -86,7 +86,7 @@ DATABASES = {
     }
 }
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=False)
 conn = psycopg2.connect('postgres://gwusdkrorfyuik:31a231a190d57f37ae2835d952bc33ab20c1a86cf064007206e28bc6b2cba676@ec2-44-195-16-34.compute-1.amazonaws.com:5432/dcq2uthp2ctbl0', sslmode='require')
 
 # Password validation
