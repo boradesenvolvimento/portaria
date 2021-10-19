524b4b9e4e0b9f78fcd6a9fff41add5cd8fb4749
cannavina98 <recannavina@hotmail.com>
2021-10-19 13:51:58 -0300

    ...

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,11 +37,11 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # SECURITY WARNING: don't run with debug turned on in production!
 
 #develop
-#DEBUG = True
+DEBUG = True
 #ALLOWED_HOSTS = []
 
 #production
-DEBUG = False
+#DEBUG = False
 ALLOWED_HOSTS = ["www.bora.kinghost.net","www.bora.kinghost.net/portaria", "bora.kinghost.net/portaria"]
 
 # Application definition
