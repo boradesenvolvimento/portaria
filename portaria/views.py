6024f12e9726d068ada1aee3a16246ab8d93d3a9
boradesenvolvimento <recannavina@hotmail.com>
2022-06-15 15:42:53 -0300

    ...

--- portaria/views.py
+++ portaria/views.py
@@ -188,26 +188,21 @@ def outputs(request):
 
 
 def paleteview(request):
+    keyga = {k:v for k,v in GARAGEM_CHOICES}
     tp_fil = GARAGEM_CHOICES
     tp_emp = Cliente.objects.values_list('razao_social', flat=True)
-    form = PaleteControl.objects.values('loc_atual').annotate(pbr=Count('id', filter=Q(tp_palete='PBR')),chep=Count('id', filter=Q(tp_palete='CHEP'))).annotate(total=ExpressionWrapper(Count('id'), output_field=IntegerField()))
+    form = PaleteControl.objects.values('loc_atual').\
+        annotate(pbr=Count('id', filter=Q(tp_palete='PBR')),chep=Count('id', filter=Q(tp_palete='CHEP'))).\
+        annotate(total=ExpressionWrapper(Count('id'), output_field=IntegerField()))
     ttcount = form.aggregate(total_amount=Sum('total'))
     fil = request.GET.get('filial')
-    tp_p = request.GET.get('tp_palete')
-    #switch case moral
-    if fil and tp_p:
-        #busca todos os campos
-        form = PaleteControl.objects.filter(loc_atual=fil, tp_palete=tp_p).values('loc_atual').annotate(pbr=Count('id', filter=Q(tp_palete='PBR')),chep=Count('id', filter=Q(tp_palete='CHEP'))).annotate(total=ExpressionWrapper(Count('id'), output_field=IntegerField()))
+    if fil:
+        form = PaleteControl.objects.filter(loc_atual=keyga[fil]).values('loc_atual').\
+            annotate(pbr=Count('id', filter=Q(tp_palete='PBR')),chep=Count('id', filter=Q(tp_palete='CHEP'))).\
+            annotate(total=ExpressionWrapper(Count('id'), output_field=IntegerField()))
         ttcount = form.aggregate(total_amount=Sum('total'))
-    elif fil and not tp_p:
-        #busca somente filial
-        form = PaleteControl.objects.filter(loc_atual=fil).values('loc_atual').annotate(pbr=Count('id', filter=Q(tp_palete='PBR')),chep=Count('id', filter=Q(tp_palete='CHEP'))).annotate(total=ExpressionWrapper(Count('id'), output_field=IntegerField()))
-        ttcount = form.aggregate(total_amount=Sum('total'))
-    elif tp_p and not fil:
-        #busca somente tipo do palete
-        form = PaleteControl.objects.filter(tp_palete=tp_p).values('loc_atual').annotate(pbr=Count('id', filter=Q(tp_palete='PBR')),chep=Count('id', filter=Q(tp_palete='CHEP'))).annotate(total=ExpressionWrapper(Count('id'), output_field=IntegerField()))
-        ttcount = form.aggregate(total_amount=Sum('total'))
-    return render(request, 'portaria/palete/paletes.html', {'form':form,'tp_fil':tp_fil,'tp_emp':tp_emp,'ttcount':ttcount})
+    return render(request, 'portaria/palete/paletes.html', {'form':form,'tp_fil':tp_fil,'tp_emp':tp_emp,
+                                                            'ttcount':ttcount})
 
 def cadpaletes(request):
     tp_fil = GARAGEM_CHOICES
