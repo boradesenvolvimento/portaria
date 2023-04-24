26b34112cab3b158a41e66fbc6f87771d39d9d20
Davi <davi.ribeirodosanjos@gmail.com>
2023-04-24 16:11:53 -0300

    comentando email

--- portaria/views.py
+++ portaria/views.py
@@ -4188,9 +4188,9 @@ def edit_compras(request, id):
                 "observacao": obj.obs,
                 "entradas": entradas,
             }
-            envio = envia_email(corpo_email)
-            if envio: messages.info(request, f'Email enviado com sucesso!')
-            else: messages.info(request, f'Ocorreu uma falha ao enviar o email')
+            # envio = envia_email(corpo_email)
+            # if envio: messages.info(request, f'Email enviado com sucesso!')
+            # else: messages.info(request, f'Ocorreu uma falha ao enviar o email')
 
         except Exception as e:
             print(f'err:{e}, err_t:{type(e).__name__}')
